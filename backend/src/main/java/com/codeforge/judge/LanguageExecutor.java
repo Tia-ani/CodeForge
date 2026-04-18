b404793f3494f5e08cde49ae478b6219a9e16f44
Tia-ani <25anishkakhurana@gmail.com>
2026-04-18 17:13:11 +0530

    chore(backend): remove legacy spring boot implementation

--- backend/src/main/java/com/codeforge/judge/LanguageExecutor.java
+++
@@ -1,9 +0,0 @@
-package com.codeforge.judge;
-
-import com.codeforge.model.TestCase;
-import java.util.List;
-
-public interface LanguageExecutor {
-    boolean compile(String code);
-    EvaluationResult run(String code, List<TestCase> testCases);
-}
--- backend/src/main/java/com/codeforge/repository/ProblemRepository.java
+++
@@ -1,7 +0,0 @@
-package com.codeforge.repository;
-
-import com.codeforge.model.Problem;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProblemRepository extends JpaRepository<Problem, Long> {
-}
